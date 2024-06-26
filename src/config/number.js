f206769c516035309a717501dd488e8d4b00b6ed
Madmkisii <138007129+Madmkisii@users.noreply.github.com>
2024-06-26 19:24:04 +0300

    𝚳𝚰𝚴𝐃𝐒𝚵𝚻_𝚸𝐋𝚫𝐘𝚵𝚪

--- src/config/number.js
+++ src/config/number.js
@@ -1,9 +1,9 @@
 /** 
- *  Created By Muhammad Adriansyah
- *  CopyRight 2024 MIT License
- *  My Github : https://github.com/xyzencode
- *  My Instagram : https://instagram.com/xyzencode
- *  My Youtube : https://youtube.com/@xyzencode
+ *  Created By Mindset_player and Kim
+ *  CopyRight 2024 UCHIHA CLAN
+ *  My Github : https://github.com/Madmkisii
+ *  My Instagram : https://instagram.com/saske_kim
+ *  My Youtube : https://youtube.com/@saske_kim
 */
 
 import { fileURLToPath } from "url";
@@ -11,7 +11,7 @@ import { watchFile, unwatchFile } from "fs";
 
 export default {
     bot: "628xxxxxxxxx",
-    owner: ["6289513081052"],
+    owner: ["25479059618"],
 }
 
 let fileP = fileURLToPath(import.meta.url);
